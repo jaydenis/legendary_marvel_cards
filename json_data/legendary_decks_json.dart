9f6af4d7259e6296911dcc7bfad1ab020c4cd2cb
Jay <jaydenis@users.noreply.github.com>
2021-11-22 08:29:06 -0500

    adding json data files

---
+++ json_data/legendary_decks_json.dart
@@ -0,0 +1,44 @@
+List legendaryDecks = [
+  {
+    "deckId":1,
+    "setId": 1,
+    "deckImage": "images/cards/heroes/captain-america-01.jpg",
+    "deckName": "Captain America",
+    "deckType": "Hero"
+  },
+  {
+    "deckId":2,
+    "setId": 1,
+    "deckImage": "images/cards/heroes/iron-man-01.jpg",
+    "deckName": "Iron Man",
+    "deckType": "Hero"
+  },
+  {
+    "deckId":3,
+    "setId": 1,
+    "deckImage": "images/cards/heroes/black-widow-01.jpg",
+    "deckName": "Black Widow",
+    "deckType": "Hero"
+  },
+  {
+    "deckId":4,
+    "setId": 1,
+    "deckImage": "images/cards/heroes/hawkeye-01.jpg",
+    "deckName": "Hawkeye",
+    "deckType": "Hero"
+  },
+  {
+    "deckId":20,
+    "setId": 2,
+    "deckImage": "images/cards/heroes/mr-fantastic-01.jpg",
+    "deckName": "Mr. Fantastic",
+    "deckType": "Hero"
+  },
+  {
+    "deckId":21,
+    "setId": 2,
+    "deckImage": "images/cards/heroes/thing-01.jpg",
+    "deckName": "The Thing",
+    "deckType": "Hero"
+  },
+];
